201c570a1af888ef0ec9c755749392ef6c5fa3c3
opeeny <godfreyopeeny@gmail.com>
2020-03-22 16:05:24 +0000

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
+  state = {
+    rows:[],
+  }
+
+  movies = [
+    {id: 0, title: 'Avenger', overview: 'As the avengers and their allies have conti'},
+    {id: 1, title: 'The avengers', overview: 'As the avengers '},
+  ]
+
+  movieRows = [];
+
+
+  render() {
+
+    return (
+      <div className = "App">
+       
+        <table className = "titleBar">
+          <tbody>
+            <tr>
+              <td><img  width = "50" src = "https://opeeny.github.io/images/opeeny.jpg" alt = "opeeny app icon"/></td>
+              <td width = "8"/>
+              <td><h1>Movie DB search</h1></td>
+            </tr>
+          </tbody>
+        </table>
+        <input style = {{fontSize: 24, display: 'block', width: '100%', paddingTop: 8, paddingBottom: 8, paddingLeft: 16}} placeholder = "Enter search term"/>
+        {/* {this.state.rows} */}
+       
+        {  this.movies.forEach(movie =>{
+           console.log(movie.title);
+          // console.log('Before', this.movieRows);
+          this.movieRows.push(<p key = {movie.id}>{movie.title}</p>);
+          console.log('Now', this.movieRows);
+        })}
+        {this.movieRows} 
+      </div>
+    )
+  }
+}
 export default App;
